118e27ee55610cd717491f9edeebaaf4b5b8b278
phyo57 <114519422+phyo57@users.noreply.github.com>
2022-09-28 00:54:38 +0630

    Add files via upload

---
+++ express.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+//static files
+app.use(express.static('assignment'))
+
+//set views
+app.set('view engine', 'ejs' )
+
+app.get("/red", (req,res) => {
+    console.log("red")
+    res.sendFile(__dirname + "/assignment/red.html")
+})
+
+app.get("/green",(req,res) => {
+    console.log("green")
+    res.sendFile(__dirname + "/assignment/green.html")
+})
+
+app.get("/yellow",(req,res) => {
+    console.log("yellow")
+    res.sendFile(__dirname + "/assignment/yellow.html")
+})
+
+app.get("/black",(req,res) => {
+    console.log("black")
+    res.sendFile(__dirname + "/assignment/black.html")
+})
+
+app.listen(3000)
